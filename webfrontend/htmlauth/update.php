5105340dcff402f232130dad00645f4276c155dd
Christian Fenzl <fenzl@t-r-t.at>
2020-01-20 16:28:02 +0100

    Replace static paths in daemon and update.php

--- webfrontend/htmlauth/update.php
+++ webfrontend/htmlauth/update.php
@@ -2,7 +2,7 @@
 require_once "loxberry_system.php";
 
 echo hallo;
-shell_exec("sh /opt/loxberry/webfrontend/cgi/plugins/alex2lox/update.sh");
+shell_exec("sh $lbphtmlauthdir/update.sh");
 echo hallo;
  header('Location: ./switch1.php'); 
 
