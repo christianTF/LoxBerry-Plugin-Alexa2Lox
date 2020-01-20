ff90505ff2781ef01552ef84f21917e3c47b04b0
Christian Fenzl <fenzl@t-r-t.at>
2020-01-20 14:09:53 +0100

    Fixes update.sh

--- webfrontend/cgi/update.sh
+++ webfrontend/cgi/update.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 #
 
-home="/opt/loxberry/webfrontend/cgi/plugins/alex2lox"
+home="REPLACELBPHTMLAUTHDIR"
 
-cp $home/alexa_remote_control.sh $home/alexa_remote_control.backup
+unlink $home/alexa_remote_control.backup
+mv $home/alexa_remote_control.sh $home/alexa_remote_control.backup
 
-wget https://loetzimmer.de/patches/alexa_remote_control.sh 
+wget https://raw.githubusercontent.com/thorsten-gehrig/alexa-remote-control/master/alexa_remote_control.sh
 
-chmod a+x alexa_remote_control.sh
+if [ ! -e $home/alexa_remote_control.sh ]; then
+	mv $home/alexa_remote_control.backup $home/alexa_remote_control.sh
+fi
+
+chmod +x $home/alexa_remote_control.sh
