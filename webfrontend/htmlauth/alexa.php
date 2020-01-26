830a8a84674f675b531d56e003be19a9ace60231
Christian Fenzl <fenzl@t-r-t.at>
2020-01-26 11:03:13 +0100

    alexa.php: Correctly redirect parameters if original is set

--- webfrontend/htmlauth/alexa.php
+++ webfrontend/htmlauth/alexa.php
@@ -12,9 +12,23 @@ echo("B&B Technik OG\n");
 
 $params = $_GET;
 
+if( isset($_GET['original']) ) {
+	$use_original=true;
+	$commandline = "--original ";
+} else {
+	$use_original=false;
+}
+
 foreach( $params as $param => $value ) {
+	if($param == "original") {
+		continue;
+	}
 	echo "$param";
-	$commandline .= "--$param ";
+	if($use_original) {
+		$commandline .= "$param ";
+	} else {
+		$commandline .= "--$param ";
+	}
 	if(!empty($value)) {
 		echo " --> $value";
 		$commandline .= "\"$value\" ";
